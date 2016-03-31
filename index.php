176dea0b8d77e392b750fbb89ef9493c4b376cd0
Cp <yoshyparrott@gmail.com>
2016-03-31 13:13:27 +0200

    Added some features and changed header img

--- index.php
+++ index.php
@@ -1,8 +1,18 @@
 <?php include 'header.php'; ?>
-<div class="container">
-  <div class="jumbotron" id='topotron'>
-    <h1>Amazing~!</h1>
+<div class="jumbotron" id='topotron'>
+  <h1>Amazing~!</h1>
+</div>
+<div class='container'>
+  <div class='row'>
+    <div class='col-md-4 col-xs-12'>
+      <h2>Features are here!</h2>
+    </div>
+    <div class='col-md-4 col-xs-12'>
+      <h2>Features are here!</h2>
+    </div>
+    <div class='col-md-4 col-xs-12'>
+      <h2>Features are here!</h2>
+    </div>
   </div>
 </div>
-
 <?php include 'footer.php'; ?>
