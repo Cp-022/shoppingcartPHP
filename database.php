6a01b756c933a16b98b61696658aae3096ae66de
Cp <yoshyparrott@gmail.com>
2016-03-31 13:41:52 +0200

    Added pdo connection

--- database.php
+++ database.php
@@ -1,6 +1,18 @@
 <?php
 $database_config = parse_ini_file("config.ini");
-echo "<pre>";
-print_r( $database_config);
-echo "</pre>"
+//echo "<pre>";
+//print_r( $database_config);
+//echo "</pre>"
+try {
+  $conn = new PDO("mysql:host=".$database_config['host'].
+  ";dbname=".$database_config['database'],
+  $database_config['user'],
+  $database_config['password']);
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  echo "Connected successfully";
+}
+catch(PDOException $e)
+{
+  echo "Connection failed: " . $e->getMessage();
+}
 ?>
