ee0c2c53a16eb27010870cfa19cc280de6d675ad
Cp <yoshyparrott@gmail.com>
2016-04-06 09:52:06 +0200

    Fixed mon issue

--- cart.php
+++ cart.php
@@ -9,9 +9,20 @@ if (isset ( $_GET['pid'])){
     $_SESSION['cart_content'] = $_GET['pid'];
   }
   else{
-    $_SESSION['cart-content'] . =',';
+    $_SESSION['cart-content'] .=',';
     $_SESSION['cart_content'] .=$GET['pid'];
   }
+}
+
+if (isset ($_GET['empty'])){
+  session_destroy();
+  header('location:cart.php');
+}
+
+if ($database_config['debug']){
+  echo "<pre>";
+  print_r ($_SESSION);
+  echo "</pre>";
 }
 ?>
 <!-- BODY ABOVE -->
